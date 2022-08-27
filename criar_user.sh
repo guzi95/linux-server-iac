7fc2c2b40a5737b2d7ba30b8f874beaa43ee06f5
guzi95 <julianna.furtado@gmail.com>
2022-08-27 23:59:54 +0000

    Arquivos IaC v.1

---
+++ criar_user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Criando usuários do sistema..."
+
+useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl pas>
+passwd guest10 -e
+
+useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl pas>
+passwd guest11 -e
+
+useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd guest12 -e
+
+
+echo "Finalizado!!"
